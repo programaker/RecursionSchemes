86e6e608b15360b75b5f5745c2b14f8f3124041d
programaker <programaker@gmail.com>
2020-04-09 23:41:06 +0200

    Improve comments

--- src/main/scala/recursionschemes/v2/MainV2.scala
+++ src/main/scala/recursionschemes/v2/MainV2.scala
@@ -3,7 +3,9 @@ package recursionschemes.v2
 object MainV2 {
   def main(args: Array[String]): Unit = {
     // x² + 3x + 4 (x² = x * x)
+    // using the smart constructors here to build the expression
     val expr = plus(times(variable("x"), variable("x")), plus(times(constant(3), variable("x")), constant(4)))
+
     val vars = Map("x" -> 2)
 
     println(cata(expr, evalToInt(_: ExprF[Int], vars))) //type inference let me down here =(
